5874cd6969c5b3a8ee5ffc2f9cf093dd7f686d5c
niklaskoopmann <koopmann.niklas@gmail.com>
2019-10-29 22:00:59 +0100

    some more voice recognition

--- Scripts/voice_recognition.py
+++ Scripts/voice_recognition.py
@@ -1,10 +1,26 @@
 import speech_recognition as sr
 
-# obtain audio from the microphone
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+m = sr.Microphone()
 
-if(r.recognize_sphinx(audio) == 'start'):
-    print("Mars Rover started!")
+try:
+    print("A moment of silence, please...")
+    with m as source: r.adjust_for_ambient_noise(source)
+    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    while True:
+        print("Say something!")
+        with m as source: audio = r.listen(source, None, 2)
+        print("Got it! Now to recognize it...")
+        try:
+            # recognize speech using Google Speech Recognition
+            value = r.recognize_sphinx(audio)
+
+            print("You said {}".format(value))
+        except sr.UnknownValueError:
+            print("Oops! Didn't catch that")
+        except sr.RequestError as e:
+            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+except KeyboardInterrupt:
+    pass
+
+# Idee: Mars Rover soll mit "Yes, master" antworten!
