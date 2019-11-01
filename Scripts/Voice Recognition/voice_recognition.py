fc8bb18c34f5e4d85be1114713c3f13d37ea83d3
niklaskoopmann <koopmann.niklas@gmail.com>
2019-11-01 22:46:29 +0100

    voice recognition: hotword added, better console logging

--- Scripts/Voice Recognition/voice_recognition.py
+++ Scripts/Voice Recognition/voice_recognition.py
@@ -2,25 +2,32 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 m = sr.Microphone()
+snowboy_config = ("./Snowboy/ubuntu1404-x86_64-1.1.1", ["./Snowboy/Hey Rover.pmdl"])
+
+def drive_forward():
+    print("[DRIVE] Yes, Master! Driving...")
+
+def stop_driving():
+    print("[DRIVE] Yes, Master! Stopping...")
+
+def turn_left():
+    print("[DRIVE] Yes, Master! Turning left...")
+
+def turn_right():
+    print("[DRIVE] Yes, Master! Turning right...")
 
 try:
-    print("A moment of silence, please...")
     with m as source: r.adjust_for_ambient_noise(source)
-    print("Set minimum energy threshold to {}".format(r.energy_threshold))
+    print("[VOICE] Set minimum energy threshold to {}".format(r.energy_threshold))
+    print("[VOICE] Say something!")
     while True:
-        print("Say something!")
-        with m as source: audio = r.listen(source, None, 2)
-        print("Got it! Now to recognize it...")
+        with m as source: audio = r.listen(source, None, 2, snowboy_config)
         try:
-            # recognize speech using Google Speech Recognition
+            # recognize speech using Sphinx to interpret locally
             value = r.recognize_sphinx(audio)
-
-            print("You said {}".format(value))
         except sr.UnknownValueError:
-            print("Oops! Didn't catch that")
+            print("[VOICE] [ERROR] Unknown Value!")
         except sr.RequestError as e:
-            print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+            print("[VOICE] [ERROR] Speech Recognition Engine - Request not fulfilled; {0}".format(e))
 except KeyboardInterrupt:
     pass
-
-# Idee: Mars Rover soll mit "Yes, master" antworten!
