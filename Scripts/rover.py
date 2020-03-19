320012dde24859ec119fcbc4fd30e41103b4b2aa
niklaskoopmann <koopmann.niklas@gmail.com>
2020-03-19 12:10:27 +0100

    current state

--- Scripts/rover.py
+++ Scripts/rover.py
@@ -1,7 +1,9 @@
 from Voice_Recognition.voice_recognition import recognize
 from Voice_Output.speak import speak
 from Actuator import actuator_control
+import math
 
-print ("Hello World!")
+print("Hello World!")
 
 recognize()
+
