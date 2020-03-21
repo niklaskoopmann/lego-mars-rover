92a5be080ec42481b81c2009c0852d041bb060e3
Niklas Koopmann <koopmann.niklas@gmail.com>
2020-03-21 10:00:17 +0000

    parameters for speech output

--- Scripts/Voice_Output/speak.py
+++ Scripts/Voice_Output/speak.py
@@ -1,9 +1,20 @@
 import pyttsx3
 
+engine = pyttsx3.init('espeak')
 
-engine = pyttsx3.init('dummy')
+# volume as float from 0 to 1
+engine.setProperty('volume',1.0)
 
+# rate (words per minute) has to be an int between 1 and 200
+engine.setProperty('rate',150)
 
 def speak(text_to_say):
     engine.say(text_to_say)
     engine.runAndWait()
+
+speak("Nice tits, get in the truck.")
+#speak("Yes, Master.")
+#speak("Water found!")
+#speak("The quick brown fox jumped over the lazy dog.")
+
+engine.stop()
