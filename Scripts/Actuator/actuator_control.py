ad3f6aca808570a79a7e836fcc3e43950dc9c8ba
niklaskoopmann <koopmann.niklas@gmail.com>
2020-05-01 20:38:25 +0200

    finalized scripts and documentation

--- Scripts/Actuator/actuator_control.py
+++ Scripts/Actuator/actuator_control.py
@@ -1,6 +1,29 @@
-import brickpi3
+#!/usr/bin/env python
+
+"""
+actuator_control.py
+This module of Mars Science Laboratory Curiosity Rover (DHBW Mosbach) implements
+all the functions necessary to run the rover. Driving, stopping and rotating are
+implemented as documented.
+"""
+
+__author__ = "Niklas Koopmann"
+__email__ = "nik.koopmann.17@lehre.mosbach.dhbw.de"
+__version__ = "1.0.0"
+__maintainer__ = "TBD"
+__status__ = "Production"
+
 import time
+
+import brickpi3
 import config
+
+#
+# TODO:
+# - Create lists of motor groups and iterate over it whenever suitable.
+# - Add sleep duration as parameter to set_steering_orientation
+# - Add factors for wheel power to drive_forward
+#
 
 print("[DRIVE] Setting up actuator controls...")
 
@@ -24,81 +47,66 @@ BP_steer.reset_all()
 BP_drive_m.reset_all()
 
 # set power limit (param 2, in per cent) and speed limit (param 3, in dps) for
-# motors
+# all driving motors
 BP_drive_fr.set_motor_limits(BP_drive_fr.PORT_A, config.MOTOR_POWER_LIMIT, 200)
 BP_drive_fr.set_motor_limits(BP_drive_fr.PORT_B, config.MOTOR_POWER_LIMIT, 200)
 BP_drive_fr.set_motor_limits(BP_drive_fr.PORT_C, config.MOTOR_POWER_LIMIT, 200)
 BP_drive_fr.set_motor_limits(BP_drive_fr.PORT_D, config.MOTOR_POWER_LIMIT, 200)
+BP_drive_m.set_motor_limits(BP_drive_m.PORT_A, config.MOTOR_POWER_LIMIT, 200)
+BP_drive_m.set_motor_limits(BP_drive_m.PORT_D, config.MOTOR_POWER_LIMIT, 200)
+
+# set power limit (param 2, in per cent) and speed limit (param 3, in dps) for
+# all steering motors -> way higher max dps because of high friction when
+# turning wheels under load
 BP_steer.set_motor_limits(BP_steer.PORT_A, config.MOTOR_POWER_LIMIT, 900)
 BP_steer.set_motor_limits(BP_steer.PORT_B, config.MOTOR_POWER_LIMIT, 900)
 BP_steer.set_motor_limits(BP_steer.PORT_C, config.MOTOR_POWER_LIMIT, 900)
 BP_steer.set_motor_limits(BP_steer.PORT_D, config.MOTOR_POWER_LIMIT, 900)
-BP_drive_m.set_motor_limits(BP_drive_m.PORT_A, config.MOTOR_POWER_LIMIT, 200)
-BP_drive_m.set_motor_limits(BP_drive_m.PORT_D, config.MOTOR_POWER_LIMIT, 200)
 
-# adjust steering motors to straight
-BP_steer.set_motor_position(BP_steer.PORT_A, 0)
-BP_steer.set_motor_position(BP_steer.PORT_B, 0)
-BP_steer.set_motor_position(BP_steer.PORT_C, 0)
-BP_steer.set_motor_position(BP_steer.PORT_D, 0)
+# output all four motor orientations at startup for debug purposes
+orientation_c = BP_steer.get_motor_encoder(BP_steer.PORT_C)
+orientation_a = BP_steer.get_motor_encoder(BP_steer.PORT_A)
+orientation_b = BP_steer.get_motor_encoder(BP_steer.PORT_B)
+orientation_d = BP_steer.get_motor_encoder(BP_steer.PORT_D)
+print("[DEBUG] A: %s; B: %s; C: %s; D: %s" %
+      (orientation_a, orientation_b, orientation_c, orientation_d))
 
+
+def set_steering_orientation(deg_a, deg_b, deg_c, deg_d):
+    BP_steer.set_motor_position(BP_steer.PORT_A, deg_a)
+    BP_steer.set_motor_position(BP_steer.PORT_B, deg_b)
+    BP_steer.set_motor_position(BP_steer.PORT_C, deg_c)
+    BP_steer.set_motor_position(BP_steer.PORT_D, deg_d)
+    time.sleep(0.5)
+
+
+# adjust all steering motors to straight position (0 degrees if assembled
+# correctly)
+set_steering_orientation(0, 0, 0, 0)
+
+# If the steering motors are already close to a 0 degree orientation, the above
+# functions do not take long. Change the time to sleep according to the time it
+# will take to rotate to 0.
 time.sleep(0.1)
 
 print("[DRIVE] All BrickPis connected and running!")
 
 
-def reset_all_motors(BP):
-    BP.reset_motor_encoder(BP.PORT_A)
-    BP.reset_motor_encoder(BP.PORT_B)
-    BP.reset_motor_encoder(BP.PORT_C)
-    BP.reset_motor_encoder(BP.PORT_D)
-
-
-def turn_off_all_wheels(BP):
-    BP.set_motor_power(BP.PORT_A, 0)
-    BP.set_motor_power(BP.PORT_B, 0)
-    BP.set_motor_power(BP.PORT_C, 0)
-    BP.set_motor_power(BP.PORT_D, 0)
-    BP.set_motor_dps(BP.PORT_A, 0)
-    BP.set_motor_dps(BP.PORT_B, 0)
-    BP.set_motor_dps(BP.PORT_C, 0)
-    BP.set_motor_dps(BP.PORT_D, 0)
-
-
-def drive_forward(support_direction=""):
+def drive_forward():
     print("[DRIVE] Yes, Master! Driving...")
-    
-    if support_direction == "right":
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_A, -config.MOTOR_TARGET_POWER * 2)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_B, -config.MOTOR_TARGET_POWER * 2)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_C, config.MOTOR_TARGET_POWER)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_D, config.MOTOR_TARGET_POWER)
-        BP_drive_m.set_motor_power(BP_drive_m.PORT_A, -config.MOTOR_TARGET_POWER)
-        BP_drive_m.set_motor_power(BP_drive_m.PORT_D, 0)
-    elif support_direction == "left":
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_A, -config.MOTOR_TARGET_POWER)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_B, -config.MOTOR_TARGET_POWER)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_C, config.MOTOR_TARGET_POWER * 2)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_D, config.MOTOR_TARGET_POWER * 2)
-        BP_drive_m.set_motor_power(BP_drive_m.PORT_A, 0)
-        BP_drive_m.set_motor_power(BP_drive_m.PORT_D, config.MOTOR_TARGET_POWER)
-    else:
-        #BP_steer.set_motor_position(BP_steer.PORT_A, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_B, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_C, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_D, 0)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_A, -config.MOTOR_TARGET_POWER * 1.5)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_B, -config.MOTOR_TARGET_POWER)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_C, config.MOTOR_TARGET_POWER)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_D, config.MOTOR_TARGET_POWER * 1.5)
-        BP_drive_m.set_motor_power(BP_drive_m.PORT_A, -config.MOTOR_TARGET_POWER)
-        BP_drive_m.set_motor_power(BP_drive_m.PORT_D, config.MOTOR_TARGET_POWER)
-        #BP_steer.set_motor_position(BP_steer.PORT_A, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_B, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_C, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_D, 0)
+
+    # more power on rear wheels bc they lag behind otherwise
+    BP_drive_fr.set_motor_power(
+        BP_drive_fr.PORT_A, -config.MOTOR_TARGET_POWER * 1.5)
+    BP_drive_fr.set_motor_power(BP_drive_fr.PORT_B, -config.MOTOR_TARGET_POWER)
+    BP_drive_fr.set_motor_power(BP_drive_fr.PORT_C, config.MOTOR_TARGET_POWER)
+    BP_drive_fr.set_motor_power(
+        BP_drive_fr.PORT_D, config.MOTOR_TARGET_POWER * 1.5)
+    BP_drive_m.set_motor_power(BP_drive_m.PORT_A, -config.MOTOR_TARGET_POWER)
+    BP_drive_m.set_motor_power(BP_drive_m.PORT_D, config.MOTOR_TARGET_POWER)
 
 
+# disable all motors and wait a little
 def stop_driving():
     print("[DRIVE] Yes, Master! Stopping...")
     BP_drive_fr.set_motor_power(BP_drive_fr.PORT_A, 0)
@@ -115,159 +123,117 @@ def stop_driving():
 
 
 def turn_left():
-
     print("[DRIVE] Yes, Master! Turning left...")
 
-    # save all four motor orientations
-    orientation_a = BP_steer.get_motor_encoder(BP_steer.PORT_A)
-    orientation_b = BP_steer.get_motor_encoder(BP_steer.PORT_B)
-    orientation_c = BP_steer.get_motor_encoder(BP_steer.PORT_C)
-    orientation_d = BP_steer.get_motor_encoder(BP_steer.PORT_D)
-    
-    print("[DEBUG] A: %s; B: %s; C: %s; D: %s" % (orientation_a, orientation_b, orientation_c, orientation_d))
+    # first turn the wheels a little too far in order to overcome friction
+    # between wheel and floor
+    set_steering_orientation(75, -60, 60, -75)
+    set_steering_orientation(60, -45, 45, -60)
 
-    BP_steer.set_motor_position(BP_steer.PORT_A, 75)
-    BP_steer.set_motor_position(BP_steer.PORT_B, -60)
-    BP_steer.set_motor_position(BP_steer.PORT_C, 60)
-    BP_steer.set_motor_position(BP_steer.PORT_D, -75)
-    time.sleep(0.5)
-    BP_steer.set_motor_position(BP_steer.PORT_A, 60)
-    BP_steer.set_motor_position(BP_steer.PORT_B, -45)
-    BP_steer.set_motor_position(BP_steer.PORT_C, 45)
-    BP_steer.set_motor_position(BP_steer.PORT_D, -60)
-    
+    # drive for some time (iterations * sleep) and readjust steering motor
+    # positions with every iteration
+    # With a fully loaded corpus, the wheels are not able to turn while
+    # standing! So the wheels are being driven a little - with different factors
+    # than in the drive_forward function.
     for i in range(15):
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_A, config.MOTOR_TARGET_POWER * 1.7)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_B, config.MOTOR_TARGET_POWER * 0.75)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_C, config.MOTOR_TARGET_POWER * 1)
-        BP_drive_fr.set_motor_power(BP_drive_fr.PORT_D, config.MOTOR_TARGET_POWER * 1.0)
-        BP_drive_m.set_motor_power(BP_drive_m.PORT_A, config.MOTOR_TARGET_POWER * 0.75)
-        BP_drive_m.set_motor_power(BP_drive_m.PORT_D, config.MOTOR_TARGET_POWER * 0.75)
+        BP_drive_fr.set_motor_power(
+            BP_drive_fr.PORT_A, config.MOTOR_TARGET_POWER * 1.25)
+        BP_drive_fr.set_motor_power(
+            BP_drive_fr.PORT_B, config.MOTOR_TARGET_POWER * 1.75)
+        BP_drive_fr.set_motor_power(
+            BP_drive_fr.PORT_C, config.MOTOR_TARGET_POWER * 1.5)
+        BP_drive_fr.set_motor_power(
+            BP_drive_fr.PORT_D, config.MOTOR_TARGET_POWER * 1.25)
+        BP_drive_m.set_motor_power(
+            BP_drive_m.PORT_A, config.MOTOR_TARGET_POWER * 1)
+        BP_drive_m.set_motor_power(
+            BP_drive_m.PORT_D, config.MOTOR_TARGET_POWER * 1)
         time.sleep(0.1)
+
+        # not using set_steering_orientation to avoid stops
         BP_steer.set_motor_position(BP_steer.PORT_A, 45)
         BP_steer.set_motor_position(BP_steer.PORT_B, -45)
         BP_steer.set_motor_position(BP_steer.PORT_C, 45)
         BP_steer.set_motor_position(BP_steer.PORT_D, -45)
-        
+
     stop_driving()
-    #BP_steer.set_motor_power(BP_steer.PORT_A, -config.MOTOR_TARGET_POWER * 1.5)
-    #BP_steer.set_motor_power(BP_steer.PORT_D, -config.MOTOR_TARGET_POWER * 1.5)
 
-    # front motors (B and C) need more power to turn bc of extra weight on them
-    # -> use maximum allowed power
-    #BP_steer.set_motor_power(BP_steer.PORT_B, config.MOTOR_POWER_LIMIT)
-    #BP_steer.set_motor_power(BP_steer.PORT_C, config.MOTOR_POWER_LIMIT)
+    # gradually adjust steering positions back to 0 (not using
+    # set_steering_orientation to avoid stops)
+    for i in range(3):
+        BP_steer.set_motor_position(BP_steer.PORT_A, -45 + (15 * (i + 1)))
+        BP_steer.set_motor_position(BP_steer.PORT_B, 30 - (10 * (i + 1)))
+        BP_steer.set_motor_position(BP_steer.PORT_C, -30 + (10 * (i + 1)))
+        BP_steer.set_motor_position(BP_steer.PORT_D, 45 - (15 * (i + 1)))
+        drive_forward()
+        time.sleep(0.1)
 
-    # turning to 45° takes around 0.3 seconds
-    time.sleep(0.5)
-
-    
-    
-    # drive for some time (to really turn Rover)
-    #drive_forward("left")
-    #time.sleep(1)
-    
-    #stop_driving()
-
-    # return motors to straight orientations
-    BP_steer.set_motor_position(BP_steer.PORT_A, -15)
-    BP_steer.set_motor_position(BP_steer.PORT_B, -15)
-    BP_steer.set_motor_position(BP_steer.PORT_C, 15)
-    BP_steer.set_motor_position(BP_steer.PORT_D, 15)
-    time.sleep(0.5)
-    BP_steer.set_motor_position(BP_steer.PORT_A, 0)
-    BP_steer.set_motor_position(BP_steer.PORT_B, 0)
-    BP_steer.set_motor_position(BP_steer.PORT_C, 0)
-    BP_steer.set_motor_position(BP_steer.PORT_D, 0)
-    
-    # turn off motors again
-    BP_steer.set_motor_power(BP_steer.PORT_A, 0)
-    BP_steer.set_motor_power(BP_steer.PORT_B, 0)
-    BP_steer.set_motor_power(BP_steer.PORT_C, 0)
-    BP_steer.set_motor_power(BP_steer.PORT_D, 0)
+    stop_driving()
 
 
-def turn_right(): # todo: maybe turn by 45 deg from current pos?
+def turn_right():
 
     print("[DRIVE] Yes, Master! Turning right...")
 
-    # save all four motor orientations
-    orientation_a = BP_steer.get_motor_encoder(BP_steer.PORT_A)
-    orientation_b = BP_steer.get_motor_encoder(BP_steer.PORT_B)
-    orientation_c = BP_steer.get_motor_encoder(BP_steer.PORT_C)
-    orientation_d = BP_steer.get_motor_encoder(BP_steer.PORT_D)
-    
-    print("[DEBUG] A: %s; B: %s; C: %s; D: %s" % (orientation_a, orientation_b, orientation_c, orientation_d))
+    # first turn the wheels a little too far in order to overcome friction
+    # between wheel and floor
+    set_steering_orientation(75, -60, 60, -75)
+    set_steering_orientation(60, -45, 45, -60)
 
-    BP_steer.set_motor_position(BP_steer.PORT_A, orientation_a - 45)
-    BP_steer.set_motor_position(BP_steer.PORT_B, orientation_b - 45)
-    BP_steer.set_motor_position(BP_steer.PORT_C, orientation_c - 45)
-    BP_steer.set_motor_position(BP_steer.PORT_D, orientation_d - 45)
+    # drive for some time (iterations * sleep) and readjust steering motor
+    # positions with every iteration
+    # With a fully loaded corpus, the wheels are not able to turn while
+    # standing! So the wheels are being driven a little - with different factors
+    # than in the drive_forward function.
+    for i in range(15):
+        BP_drive_fr.set_motor_power(
+            BP_drive_fr.PORT_A, -config.MOTOR_TARGET_POWER * 1.25)
+        BP_drive_fr.set_motor_power(
+            BP_drive_fr.PORT_B, -config.MOTOR_TARGET_POWER * 1.75)
+        BP_drive_fr.set_motor_power(
+            BP_drive_fr.PORT_C, -config.MOTOR_TARGET_POWER * 1.5)
+        BP_drive_fr.set_motor_power(
+            BP_drive_fr.PORT_D, -config.MOTOR_TARGET_POWER * 1.25)
+        BP_drive_m.set_motor_power(
+            BP_drive_m.PORT_A, -config.MOTOR_TARGET_POWER * 1)
+        BP_drive_m.set_motor_power(
+            BP_drive_m.PORT_D, -config.MOTOR_TARGET_POWER * 1)
+        time.sleep(0.1)
 
-    #BP_steer.set_motor_power(BP_steer.PORT_A, config.MOTOR_TARGET_POWER)
-    #BP_steer.set_motor_power(BP_steer.PORT_D, config.MOTOR_TARGET_POWER)
+        # not using set_steering_orientation to avoid stops
+        BP_steer.set_motor_position(BP_steer.PORT_A, 45)
+        BP_steer.set_motor_position(BP_steer.PORT_B, -45)
+        BP_steer.set_motor_position(BP_steer.PORT_C, 45)
+        BP_steer.set_motor_position(BP_steer.PORT_D, -45)
 
-    # front motors (B and C) need more power to turn bc of extra weight on them
-    # -> use maximum allowed power
-    #BP_steer.set_motor_power(BP_steer.PORT_B, -config.MOTOR_POWER_LIMIT)
-    #BP_steer.set_motor_power(BP_steer.PORT_C, -config.MOTOR_POWER_LIMIT)
-
-    # turning to 45° takes around 0.3 seconds
-    time.sleep(0.5)
-
-    # turn off motors again
-    BP_steer.set_motor_power(BP_steer.PORT_A, 0)
-    BP_steer.set_motor_power(BP_steer.PORT_B, 0)
-    BP_steer.set_motor_power(BP_steer.PORT_C, 0)
-    BP_steer.set_motor_power(BP_steer.PORT_D, 0)
-    
-    # drive for some time (to turn Rover)
-    drive_forward("right")
-    time.sleep(2)
     stop_driving()
 
-    # return motors to straight orientations
-    BP_steer.set_motor_position(BP_steer.PORT_A, 0)
-    BP_steer.set_motor_position(BP_steer.PORT_B, 0)
-    BP_steer.set_motor_position(BP_steer.PORT_C, 0)
-    BP_steer.set_motor_position(BP_steer.PORT_D, 0)
-    time.sleep(0.5)
+    # gradually adjust steering positions back to 0 (not using
+    # set_steering_orientation to avoid stops)
+    for i in range(3):
+        BP_steer.set_motor_position(BP_steer.PORT_A, -45 + (15 * (i + 1)))
+        BP_steer.set_motor_position(BP_steer.PORT_B, 30 - (10 * (i + 1)))
+        BP_steer.set_motor_position(BP_steer.PORT_C, -30 + (10 * (i + 1)))
+        BP_steer.set_motor_position(BP_steer.PORT_D, 45 - (15 * (i + 1)))
+        drive_forward()
+        time.sleep(0.1)
+
+    stop_driving()
 
 
 def test_drive():
     try:
-        #time.sleep(3)
-        #for i in range(10):
-         #   drive_forward()
-          #  time.sleep(0.1)
-        #stop_driving()
-        BP_steer.set_motor_position(BP_steer.PORT_A, 0)
-        BP_steer.set_motor_position(BP_steer.PORT_B, 0)
-        BP_steer.set_motor_position(BP_steer.PORT_C, 0)
-        BP_steer.set_motor_position(BP_steer.PORT_D, 0)
-        time.sleep(0.5)
+        set_steering_orientation(0, 0, 0, 0)
+        time.sleep(3)
+        drive_forward()
+        time.sleep(1)
+        stop_driving()
         turn_left()
+        turn_right()
+        drive_forward()
+        time.sleep(1)
         stop_driving()
-        for i in range(3):
-            #stop_driving()
-            BP_steer.set_motor_position(BP_steer.PORT_A, -45+(15*(i+1)))
-            BP_steer.set_motor_position(BP_steer.PORT_B, 30-(10*(i+1)))
-            BP_steer.set_motor_position(BP_steer.PORT_C, -30+(10*(i+1)))
-            BP_steer.set_motor_position(BP_steer.PORT_D, 45-(15*(i+1)))
-            drive_forward()
-            time.sleep(0.1)
-        stop_driving()
-        #BP_steer.set_motor_position(BP_steer.PORT_A, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_B, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_C, 0)
-        #BP_steer.set_motor_position(BP_steer.PORT_D, 0)
-        #stop_driving()
-        #turn_right()
-        #stop_driving()
     except brickpi3.FirmwareVersionError as error:
         print(error)
     except:
         print("Communication with BrickPi3 unsuccessful")
-        
-
-test_drive()
